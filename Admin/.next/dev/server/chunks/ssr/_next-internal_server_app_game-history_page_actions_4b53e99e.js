69cd9e091e3dc94e9059958b74eb82796d8f169f
actually-Josiah <JosiahIsCoding@gmail.com>
2026-01-22 15:23:04 +0000

    Delete Admin/.next/dev directory

--- Admin/.next/dev/server/chunks/ssr/_next-internal_server_app_game-history_page_actions_4b53e99e.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/game-history/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_game-history_page_actions_4b53e99e.js.map
--- Admin/.next/dev/server/chunks/ssr/_next-internal_server_app_give-tokens_page_actions_fd315f59.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/give-tokens/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_give-tokens_page_actions_fd315f59.js.map
